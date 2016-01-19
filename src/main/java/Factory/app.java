e968e5de9a1862c9b23c88774a4910f8a591103c
Michal Stepniak <veryintelligentperson@gmail.com>
2016-01-19 01:33:18 +0100

    abstractFactory

---
+++ src/main/java/Factory/app.java
@@ -0,0 +1,10 @@
+package Factory;
+
+/**
+ * Created by root on 19.01.16.
+ */
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
